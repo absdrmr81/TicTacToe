da46f665e1985b7d4612fcbe664d6aaaf5619435
Abel E <absdrmr81@yahoo.com>
2014-03-13 23:01:11 -0500

    second commit

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,21 @@
 
 @interface ViewController ()
 
+{
+    IBOutlet UILabel *myLabelOne;
+    IBOutlet UILabel *myLabelTwo;
+    IBOutlet UILabel *myLabelThree;
+    IBOutlet UILabel *myLabelFour;
+    IBOutlet UILabel *myLabelFive;
+    IBOutlet UILabel *myLabelSix;
+    IBOutlet UILabel *myLabelSeven;
+    IBOutlet UILabel *myLabelEight;
+    IBOutlet UILabel *myLabelNine;
+    IBOutlet UILabel *whichPlayerLabel;
+}
+
+@property (nonatomic, strong) NSArray *allLabels;
+
 @end
 
 @implementation ViewController
@@ -17,13 +32,139 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    whichPlayerLabel.text = @"Player 1";
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGestureRecognizer
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGPoint point = [tapGestureRecognizer locationInView:self.view];
+  
+    
+    
+    
+    
+    
+    //  [self findLabelUsingPoint:point];
+    
+    //point.x += mylabelOne.center.x;
+   // point.y += myLabelOne.center.y;
+    
+    if (CGRectContainsPoint(myLabelOne.frame, point))
+    {
+        myLabelOne.text = @"X";
+        myLabelOne.textColor = [UIColor blueColor];
+        [myLabelOne sizeToFit];
+      
+        if (CGRectContainsPoint(myLabelOne.frame, point))
+        {
+            myLabelOne.text = @"X";
+            myLabelOne.textColor = [UIColor blueColor];
+            [myLabelOne sizeToFit];
+    
 }
+        
+#pragma mark myUSERLABEL
+        
+- (BOOL) labelFieldShouldReturn:(UILabel *)whichPlayerLabel
+        
+        NSString* firstString = @"Player 1";
+        NSString* secondString = @"Player 2";
+        
+    if ([whichPlayerLabel.text isEqualToString:firstString]
+{
+        whichPlayerLabel.text = secondString;
+}
+            else if ([whichPlayerLabel.text isEqualToString:secondString] {
+            return;
+        }
+                   else {
+            whichPlayerLabel.text = firstString;
+        }
+                   
+                   
+//
+//- (void)findLabelUsingPoint:(CGPoint)point
+//{
+//    
+//    if ([whichPlayerLabel.text isEqualToString:@"Player 1"])
+        
+        
+//    {
+//        whichPlayerLabel.text = @"Player 2";
+//    }
+//
+//    if (CGRectContainsPoint(myLabelOne.frame, point))
+//{
+//        myLabelOne.text = @"X";
+//        myLabelOne.textColor = [UIColor blueColor];
+//        [myLabelOne sizeToFit];
+//
+//        NSLog(@"myLabelOne");
+//    }
+//    
+//}
+//
+//CGPoint point = [panGestureRecognizer translationInView:self.view];
+//theFuture.transform = CGAffineTransformMakeTranslation(point.x, point.y);
+//
+//point.x += theFuture.center.x;
+//point.y += theFuture.center.y;
+//if (CGRectContainsPoint(thePrecogs.frame, point))
+//{
+//    theFuture.text = @"A Ficticious and Incriminating Future";
+//    thePrecogs.backgroundColor = [UIColor orangeColor];
+//    [theFuture sizeToFit];
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if (CGRectContainsPoint(label1.frame, point))
+
+//        {
+//            theFuture.text = @"A Ficticious and Incriminating Future";
+//            thePrecogs.backgroundColor = [UIColor orangeColor];
+//            [theFuture sizeToFit];
+//        }
+
+
+
+//
+//    - (IBAction)onDrag:(UIPanGestureRecognizer*)panGestureRecognizer
+//    {
+//        CGPoint point = [panGestureRecognizer translationInView:self.view];
+//        theFuture.transform = CGAffineTransformMakeTranslation(point.x, point.y);
+//
+//        point.x += theFuture.center.x;
+//        point.y += theFuture.center.y;
+//
+//        if (CGRectContainsPoint(thePrecogs.frame, point))
+//        {
+//            theFuture.text = @"A Ficticious and Incriminating Future";
+//            thePrecogs.backgroundColor = [UIColor orangeColor];
+//            [theFuture sizeToFit];
+//        }
+
+}
+
+
 
 @end
